c4ac0a1b3b2d7f0c1b749f3bc54c845776eaeca3
proton0 <developmentproton1@gmail.com>
2024-03-04 10:55:35 +0800

    Improve vm package

--- ProtonOS PPM/virtual_machine/main.py
+++ ProtonOS PPM/virtual_machine/main.py
@@ -4,10 +4,16 @@ init(autoreset=True)
 import __main__
 def startVM(command):
     if command[0] == "start_vm":
+        user = "root"
         while True:
-            user_input = input(f"{Fore.GREEN}root@virtual-machine{Fore.RESET} ~ > ")
-            if user_input == "exit":
+            user_input = input(f"{Fore.GREEN}{user}@virtual-machine{Fore.RESET} ~ > ").split(" ")
+            if user_input[0] == "exit":
                 return
-            print(user_input)
+            elif user_input[0] == "switch":
+                user = user_input[1]
+            elif user_input[0] == "vm":
+                print("ProtonOS Virtual Machine version 1.1")
+            else:
+                print(user_input)
 
 __main__.system_commands.append(startVM)
